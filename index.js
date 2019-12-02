aa9da91dd0305ebe397a1479b8e77fff9da673cf
Chrismis79 <chrismis79@gmail.com>
2019-12-02 17:58:51 -0600

    GET req working and returning list of users

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 // implement your API here
 const express = require("express");
-const server = express();
+const db = require("./data/db");
 
+const server = express();
+server.use(express.json());
 
 server.get('api/users', (req, res) => {
     res.send({api: "Up and running..."});
 });
 
+server.get("/api/users", (req, res) => {
+    db.find()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(error => {
+            console.log("Error on GET /api/users", error);
+            res.status(500)
+                .json({errorMessage: "Error getting list of users from database"})
+        });
+});
 
 
 const port = 5000;
